bba58e4eecbcc70f4ba937d70319e6a6b343776a
Никита Комаров <nikitakmrv76@gmail.com>
2024-02-06 16:51:39 +0300

    addded some methods for cart

--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -10,23 +10,36 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action) {
-      const isItem = state.items.find((el) => el.id == action.payload.id);
-      if (isItem) {
-        isItem.count += 1;
+      const newItem = state.items.find((el) => el.id == action.payload.id);
+      if (newItem) {
+        newItem.count += 1;
       } else {
         state.items.push(action.payload);
       }
       state.totalPrice += Number(action.payload.price);
     },
+    subFromCart(state, action) {
+      const newItem = state.items.find((el) => el.id == action.payload.id);
+      if (newItem.count !== 0) {
+        newItem.count -= 1;
+        state.totalPrice -= Number(newItem.price);
+      } else {
+        state.items = state.items.filter((el) => el.id !== action.payload.id);
+      }
+    },
     removeToCart(state, action) {
-      state.items = state.items.filter((el) => el.id !== action.payload);
+      state.items = state.items.filter((el) => el.id !== action.payload.id);
+      state.totalPrice -=
+        Number(action.payload.price) * Number(action.payload.count);
     },
     clearCart(state) {
       state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
-export const { addToCart, removeToCart, clearCart } = cartSlice.actions;
+export const { addToCart, removeToCart, clearCart, subFromCart } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
